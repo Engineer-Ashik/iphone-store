d81d74a5e3ca55d08ba857d20eda36b3ee766104
Ashik <engineerashikur@gmail.com>
2024-11-04 11:45:00 +0600

    Added my practising RKO-iPhone store Website

---
+++ phone.js
@@ -0,0 +1,33 @@
+//console.log("phone js connected");
+function phonefetch(){
+fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
+.then(response => response.json())
+.then(data => phonedata(data))
+}
+
+function phonedata(parameter){
+    const phones = parameter.data;
+    //console.log(phones);
+    const phonecContainer = document.getElementById('post-container');
+    phones.forEach(phone => {
+      console.log(phone);
+        const divCreate = document.createElement('div');
+        divCreate.classList = `border-4 border-black text-center`;
+        divCreate.innerHTML = `
+            <figure>
+              <img src="${phone.image}" alt="${phone.phone_name}"  class="w-auto"/>
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title justify-center">${phone.phone_name}!</h2>
+              <p>${phone.slug}..</p>
+              <div class="card-actions justify-center">
+                <button class="btn btn-primary bg-red-700">Buy Now</button>
+              </div>
+            </div>
+        `
+        phonecContainer.appendChild(divCreate);
+    });
+}
+
+//callback
+phonefetch();
