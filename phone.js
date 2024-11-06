8d78e40550f620d03bcc83267f09026b21e692e6
Ashik <engineerashikur@gmail.com>
2024-11-06 12:57:59 +0600

    Added a loading Button and symbolic Function

--- phone.js
+++ phone.js
@@ -4,8 +4,9 @@ fetch(`https://openapi.programming-hero.com/api/phones?search=${inputtext}`)
 .then(response => response.json())
 .then(data => phonedata(data))
 }
-  //button and input
+  //Search button and input
   document.getElementById('button-field').addEventListener('click', function calliT(){
+    loadingButton(true);
     const inputField = document.getElementById('input-field');
     const inputValue = inputField.value ;
     inputField.value = "";
@@ -29,6 +30,12 @@ function phonedata(parameter){
 
     //output only 3 rows with 12 cards
     phones = phones.slice(0,12);
+
+   // showbutton.addEventListener('click', function slicing(){
+   //   phones = parameter.data;
+   //   console.log("splice click", phones);
+   // })
+
     //console.log(phones);
     const phonecContainer = document.getElementById('post-container');
     phonecContainer.textContent = '';
@@ -48,9 +55,19 @@ function phonedata(parameter){
               </div>
             </div>
         `
+        loadingButton(false);
         phonecContainer.appendChild(divCreate);
     });
 }
+    function loadingButton(instruction){
+      const toggles = document.getElementById('loading-container');
+      if(instruction === true){
+        toggles.classList.remove('hidden');
+      }
+      else{
+        toggles.classList.add('hidden');
+      }
+    }
 
 //callback
 //phonefetch();
